20d7562e953c57d9664ad17fc0aa82336c314ea2
Wellington Garcia <garciawell@gmail.com>
2020-02-20 13:05:35 -0300

    Back version

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -1,13 +1,8 @@
-import React from "react";
-// import { shallow } from 'enzyme';
-// import Player from "../src/components/Player";
+import React from 'react';
+import { shallow } from 'enzyme';
+import Player from '../src/components/Player';
 
-// it('renders correctly', () => {
-//   const wrapper = shallow(<Player />);
-//   expect(wrapper.find('View').exists()).toBe(true);
-// });
-
-it("renders correctly", () => {
-  const wrapper = 1 + 1;
-  expect(wrapper).toBe(2);
+it('renders correctly', () => {
+  const wrapper = shallow(<Player />);
+  expect(wrapper.find('View').exists()).toBe(true);
 });
