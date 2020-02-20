65e4d12f9ade2be71bb33bce97d3ad1d66342e30
Wellington Garcia <garciawell@gmail.com>
2020-02-19 21:55:13 -0300

    babel config

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import { AppRegistry } from 'react-native';
 import TrackPlayer from 'react-native-track-player';
 import App from './src';
 import { name as appName } from './app.json';
-import player from '~/services/player';
+import player from './src/services/player';
 
 AppRegistry.registerComponent(appName, () => App);
 TrackPlayer.registerPlaybackService(() => player);
